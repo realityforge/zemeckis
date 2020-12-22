3d5c39742fe0ee5e573d4633152de931591e65b4
Peter Donald <peter@realityforge.org>
2020-12-22 16:26:18 +1100

    Add some helpers to create random data

--- core/src/test/java/zemeckis/AbstractTest.java
+++ core/src/test/java/zemeckis/AbstractTest.java
@@ -1,5 +1,6 @@
 package zemeckis;
 
+import java.util.Random;
 import javax.annotation.Nonnull;
 import org.realityforge.braincheck.BrainCheckTestUtil;
 import org.testng.annotations.AfterMethod;
@@ -10,6 +11,10 @@ import static org.testng.Assert.*;
 @Listeners( MessageCollector.class )
 public abstract class AbstractTest
 {
+  @Nonnull
+  private static final String CHARS = "abcdefghijklmnopqrstuvwxyz0123456789";
+  @Nonnull
+  private static final Random c_random = new Random();
   @Nonnull
   private final TestLogger _logger = new TestLogger();
 
@@ -39,5 +44,34 @@ public abstract class AbstractTest
                                                @Nonnull final String message )
   {
     assertEquals( expectThrows( IllegalStateException.class, throwingRunnable ).getMessage(), message );
+  }
+
+  protected final int randomInt()
+  {
+    return getRandom().nextInt();
+  }
+
+  @Nonnull
+  protected final Random getRandom()
+  {
+    return c_random;
+  }
+
+  @Nonnull
+  protected final String randomString()
+  {
+    return randomString( 12 );
+  }
+
+  @SuppressWarnings( "SameParameterValue" )
+  @Nonnull
+  protected final String randomString( final int stringLength )
+  {
+    final StringBuilder sb = new StringBuilder( stringLength );
+    for ( int i = 0; i < stringLength; i++ )
+    {
+      sb.append( CHARS.charAt( Math.abs( randomInt() % CHARS.length() ) ) );
+    }
+    return sb.toString();
   }
 }
